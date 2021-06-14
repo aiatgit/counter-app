ca6cd7fd6240e281622d395cfa4154ec7b11f908
aiatgit <abhis.innani@gmail.com>
2021-06-14 10:28:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title:string = 'Counter App';
+
+  count:number=0;
+
+  handleIncrease = ()=>{
+    this.count=this.count+1;
+    if(this.count>10){
+      this.count=10;
+    }
+  }
+
+  handleDecrease = ()=> {
+    this.count=this.count-1;
+    if(this.count<0){
+      this.count=0;
+
+    }
+    }
+
+  handleReset = ()=> {
+    this.count=0;
+  }
 }
